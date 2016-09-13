6e494b60f3f58ba892a948ec181631f5e75b0249
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2016-09-12 23:27:18 -0600

    everything working but the filter

--- main.js
+++ main.js
@@ -1,34 +1,82 @@
 $(document).ready( () => {
-	let todos = ['one', 'two'];
-	const loadTodos = () => {
+	let todos = [{ name: 'one', completed: false }, { name: 'two', completed: false }];
+	const loadAllTodos = () => {
 		$('#todos').empty();
 		let counter = 0;
 		for (let name of todos) {
-			$('#todos').append(`<li class="item">${name} || <button data-todo-id="${counter}" class="deleteItem">Delete</button></li>`);
-			counter += 1;
+			if (name.completed === false) {
+				$('#todos').append(`<div class="col s3 card">
+														<li class="item card-title" data-todo-id="${counter}">${name.name}
+														<div class="card-action"> 
+														<button data-todo-id="${counter}" class="deleteItem">Delete</button>
+														<button data-todo-id="${counter}" class="editItem">Edit</button></li>`);
+			} else {
+				$('#todos').append(`<div class="col s3 card">
+														<li class="completed card-title" data-todo-id="${counter}">${name.name} 
+														<div class="card-action"> 
+														<button data-todo-id="${counter}" class="deleteItem">Delete</button>
+														<button data-todo-id="${counter}" class="editItem">Edit</button></li>`);
+			} counter += 1;
 		}
+	}
+
+	const incomplete = () => {
+		
 	}
 
 	$('#add').on('submit', (e) => {
 		e.preventDefault();
-		let todo = $('#addTodo').val();
-		todos = [todo, ...todos];
+		let todo = { name: $('#addTodo').val(),
+								 completed: false };
+		todos = [...todos, todo];
 		$('#addTodo').val('');
-		loadTodos();
+		loadAllTodos();
 	});
 
 	$(document).on('click', '.item', (e) => {
-		$(e.target).toggleClass('completed');
+		let id = $(e.target).data('todo-id');
+		let todo = todos[id];
+		todo.completed = true;
+		loadAllTodos();
+	});
+
+	$(document).on('click', '.completed', (e) => {
+		let id = $(e.target).data('todo-id');
+		let todo = todos[id];
+		todo.completed = false;
+		loadIncompleteTodos();
 	});
 
 	$(document).on('click', '.deleteItem', (e) => {
 		let id = $(e.target).data('todo-id');
-		console.log(todos[id]);
 		todos.splice(id, 1);
-		loadTodos();
-	})
+		loadAllTodos();
+	});
 
-	loadTodos();
+	$(document).on('click', '.editItem', (e) => {
+		let id = $(e.target).data('todo-id');
+		let todoEdit = todos[id];
+		let todoName = todoEdit.name
+		$('#editTodo').val(todoName);
+		$('#editId').val(id);
+		$('#edit_form').slideDown();
+		$('#add').slideUp();
+	});
+
+	$('#edit').submit( (e) => {
+		e.preventDefault();
+		let form = e.target;
+		let id = $('#editId').val();
+		let editing = { name: $('#editTodo').val(),
+							 completed: false };
+		todos.splice(id, 1, editing);
+		loadAllTodos();
+		$('#editTodo').val('');
+		$('#edit_form').slideUp();
+		$('#add').slideDown();
+	});
+
+	loadAllTodos();
 
 
 });
